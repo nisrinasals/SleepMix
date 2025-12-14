677b72a41a2fb141bb9e34f6fbe7b25fab398c9a
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-14 23:10:46 +0700

    vm factory

---
+++ app/src/main/java/com/example/sleepmix/viewmodel/provider/ViewModelFactory.kt
@@ -0,0 +1,27 @@
+package com.example.sleepmix.viewmodel.provider
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.sleepmix.repositori.UserRepository
+import com.example.sleepmix.viewmodel.LoginViewModel
+import com.example.sleepmix.viewmodel.RegistrasiViewModel
+
+class RegistrasiViewModelFactory(private val userRepository: UserRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(RegistrasiViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return RegistrasiViewModel(userRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+class LoginViewModelFactory(private val userRepository: UserRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return LoginViewModel(userRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
