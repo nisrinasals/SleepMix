8dd4dded1842e69649c91d4d9ae2918d6e063780
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-15 16:01:55 +0700

    ganti panggil

--- app/src/main/java/com/example/sleepmix/MainActivity.kt
+++ app/src/main/java/com/example/sleepmix/MainActivity.kt
@@ -3,28 +3,17 @@ package com.example.sleepmix
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.enableEdgeToEdge
+import com.example.sleepmix.ui.navigation.SleepMixNavigation
 import com.example.sleepmix.ui.theme.SleepMixTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import com.example.sleepmix.navigation.MainNavigation
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Hapus enableEdgeToEdge() atau ganti dengan implementasi terbaru
-
+        enableEdgeToEdge()
         setContent {
             SleepMixTheme {
-                // Menggunakan Surface sebagai wadah utama dan memanggil Navigasi
-                Surface(
-                    modifier = androidx.compose.ui.Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainNavigation() // Panggil Main Navigation
-                }
+                SleepMixNavigation()
             }
         }
     }
